706bce39b66dbdc24593faf6b13addf05744e66c
dipeshkhatiwada <deepaacee@gmail.com>
2020-05-12 08:42:21 +0545

    password

--- djangoauth/urls.py
+++ djangoauth/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('signin/', views.signin, name='signin'),
     path('signout/', views.signout, name='signout'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('change-password/', views.change_password, name='change_password'),
 ]
--- djangoauth/views.py
+++ djangoauth/views.py
@@ -1,13 +1,18 @@
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
+from django.contrib.auth.forms import PasswordChangeForm
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
 
 def home(request):
+    if request.user.id:
+        return redirect('dashboard')
     return render(request, 'home.html')
 
 def signup(request):
+    if request.user.id:
+        return redirect('dashboard')
     if request.method == 'GET':
         return render(request, 'signup.html')
     else:
@@ -31,6 +36,8 @@ def signup(request):
 
 
 def signin(request):
+    if request.user.id:
+        return redirect('dashboard')
     if request.method == 'GET':
         return render(request, 'signin.html')
     else:
@@ -40,14 +47,11 @@ def signin(request):
         user = authenticate(username=u, password=p)
         if user is not None:
             login(request, user)
-            # messages.add_message(request, messages.SUCCESS, "Login success")
+            messages.add_message(request, messages.SUCCESS, "Login success")
             return redirect("dashboard")
         else:
             messages.add_message(request, messages.ERROR, "Username and Password doesn't match")
             return redirect("signin")
-
-        # else:
-        #     return HttpResponse("Password Error")
 
 
 def signout(request):
@@ -63,3 +67,43 @@ def dashboard(request):
     }
     return render(request, 'dashboard.html', context)
 
+@login_required(login_url='signin')
+def change_password(request):
+    if request.method == 'GET':
+        form = PasswordChangeForm(request.user)
+        context = {'forms': form}
+        return render(request, 'change_password.html', context)
+    else:
+        form = PasswordChangeForm(request.user, request.POST)
+        if request.POST.get('new_password1') == request.POST.get('new_password2'):
+            if form.is_valid():
+                user = form.save()
+                update_session_auth_hash(request, user)  # Important!
+                messages.add_message(request, messages.SUCCESS, "Your password is successfully updated!")
+                return redirect('change_password')
+            else:
+                messages.add_message(request, messages.ERROR, "Password doesn't match")
+                return redirect('change_password')
+
+        else:
+            messages.add_message(request, messages.ERROR, "Password and Confirm doesn't match")
+            return redirect('change_password')
+
+        # old_pass = request.POST.get('old_password')
+        # new_pass1 = request.POST.get('password1')
+        # new_pass2 = request.POST.get('password2')
+        # # u = request.POST['username']
+        # user = authenticate(username=request.user.username, password=old_pass)
+        # if user is not None:
+        #     if new_pass1 == new_pass2:
+        #         user = User.objects.get(id=request.user.id)
+        #         print(user)
+        #         user.set_password(new_pass1)
+        #         messages.add_message(request, messages.SUCCESS, "Password Changed Successfully")
+        #         return redirect("dashboard")
+        #     else:
+        #         messages.add_message(request, messages.ERROR, "Password and Confirm doesn't match")
+        #         return redirect("change_password")
+        # else:
+        #     messages.add_message(request, messages.ERROR, "Old password doesn't match")
+        #     return redirect("change_password")
